1618461d626149f9cc8dee199aaab8692cc2ae5e
Pogosyan Sos <pogosyan.sos@wb.ru>
2023-05-01 13:59:19 +0300

    add configuration and tune graceful shutdown

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,16 @@
 package main
 
 import (
+	"context"
 	"net"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/keepalive"
 
+	"github.com/ZeeeUs/gRPC-Service/internal/config"
 	pb "github.com/ZeeeUs/gRPC-Service/internal/social_network/proto"
 	"github.com/ZeeeUs/gRPC-Service/internal/social_network/repository"
 	socialNetwork "github.com/ZeeeUs/gRPC-Service/internal/social_network/transport/grpc"
@@ -21,18 +22,25 @@ import (
 func main() {
 	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
 
+	cfg, err := config.NewConfig()
+	if err != nil {
+		log.Fatal().Msgf("failed to get config: %v", err)
+	}
+
 	shutdown := make(chan os.Signal, 1)
 	signal.Notify(shutdown, os.Interrupt, syscall.SIGTERM)
 
-	listener, err := net.Listen("tcp", ":8089")
+	ctx, cancel := context.WithCancel(context.Background())
+
+	listener, err := net.Listen(cfg.GRPCConfig.Network, cfg.GRPCConfig.Address)
 	if err != nil {
 		log.Error().Err(err).Msg("can't start listener")
 	}
 
 	grpcServer := grpc.NewServer(grpc.KeepaliveParams(keepalive.ServerParameters{
-		MaxConnectionIdle: 15 * time.Minute,
-		Timeout:           30 * time.Second,
-		MaxConnectionAge:  15 * time.Minute,
+		MaxConnectionIdle: cfg.GRPCConfig.MaxConnectionIdle,
+		Timeout:           cfg.GRPCConfig.Timeout,
+		MaxConnectionAge:  cfg.GRPCConfig.MaxConnectionAge,
 	}))
 
 	repo := repository.New(log.Logger)
@@ -44,10 +52,16 @@ func main() {
 	go func() {
 		if err = grpcServer.Serve(listener); err != nil {
 			log.Error().Err(err).Msg("failed to serve gRPC server")
+			cancel()
 		}
 	}()
 
-	<-shutdown
-	log.Info().Msgf("start shutdown server")
+	select {
+	case v := <-shutdown:
+		log.Info().Msgf("start shutdown server: %v", v)
+	case done := <-ctx.Done():
+		log.Error().Msgf("ctx.Done: %v", done)
+	}
+
 	grpcServer.GracefulStop()
 }
---
+++ internal/config/config.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"time"
+
+	"github.com/spf13/viper"
+)
+
+type GRPCConfig struct {
+	Network           string
+	Address           string
+	MaxConnectionIdle time.Duration
+	Timeout           time.Duration
+	MaxConnectionAge  time.Duration
+}
+
+type Config struct {
+	GRPCConfig GRPCConfig
+}
+
+func SetupSource(cfgType string) (*viper.Viper, error) {
+	v := viper.New()
+
+	v.SetConfigType(cfgType)
+	v.AutomaticEnv()
+
+	return v, nil
+}
+
+func NewConfig() (*Config, error) {
+	v, err := SetupSource("env")
+	if err != nil {
+		return nil, err
+	}
+
+	return &Config{
+		GRPCConfig: GRPCConfig{
+			Network:           v.GetString("GRPC_NETWORK"),
+			Address:           v.GetString("GRPC_ADDRESS"),
+			MaxConnectionIdle: v.GetDuration("GRPC_MAX_CONN_IDLE"),
+			Timeout:           v.GetDuration("GRPC_SERVER_TIMEOUT"),
+			MaxConnectionAge:  v.GetDuration("GRPC_MAX_CONN_AGE"),
+		},
+	}, nil
+}
